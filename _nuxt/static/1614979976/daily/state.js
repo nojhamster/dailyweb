a5efd439be76c1afcd843050fc5d14f143727fc6
nojhamster <nojhamster@users.noreply.github.com>
2021-03-07 22:13:03 +0000

    deploy: 4f3b06f7b5a52212a50bb018c57647291056cbff

--- _nuxt/static/1614979976/daily/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdailyweb\u002F_nuxt\u002Fstatic\u002F1614979976",layout:"default",error:a,serverRendered:false,routePath:"\u002Fdaily",config:{app:{basePath:"\u002Fdailyweb\u002F",assetsPath:"\u002Fdailyweb\u002F_nuxt\u002F",cdnURL:a}}}}(null));
